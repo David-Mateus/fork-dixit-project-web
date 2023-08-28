49ae465be0e3b5541b9fda87b8dc42cd78e15dd2
David-Mateus <david-mateus09@hotmail.com>
2023-08-28 19:52:58 -0300

    update code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Home from './home'
+import Home from './components/Home'
 
 const inter = Inter({ subsets: ['latin'] })
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,36 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        gray: {
+          "100": "#fbfbfb",
+          "200": "#05020e",
+          "300": "rgba(0, 0, 0, 0.71)",
+          "400": "rgba(17, 15, 26, 0.99)",
+        },
+        white: "#fff",
+        black: "#000",
+        gainsboro: "#d9d9d9",
+      },
+      fontFamily: {
+        inter: "Inter",
+      },
+      borderRadius: {
+        "3xs": "10px",
+      },
+
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    fontSize: {
+      inherit: "inherit",
+    },
+  },
+  corePlugins: {
+    preflight: false,
   },
   plugins: [],
 }
